d95b0244c223d3cb73c31d24fdde77070d7334f0
dede-kuntoro <50207351+dede-kuntoro@users.noreply.github.com>
2019-05-24 13:13:02 +0700

    Add files via upload

---
+++ cinta.sh
@@ -0,0 +1,55 @@
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+sleep 0.01
+cd
+clear
+sleep 0.01
+
+
+
+
+echo  " $green    Hai Sayang apa kabar!!!"
+
+
+
+
+
+
+
+
+
+
+cd $HOME/Cinta
+python .cinta.py
+
+
+
+
+
+sleep 0.01
+echo "$red aku menyukaimu bukan dari apa yang aku lihat"
+echo "$white melainkan dari apa yang aku rasakan"
+sleep 0.01
+
+
+figlet "Aku" | lolcat
+figlet "Sayang" | lolcat
+figlet "Kamu" | lolcat
+date | lolcat
+echo "IG: @dedekuntoro" | lolcat
+echo ""
+echo "JANGAN SAMPAI DRAMA DI SOSMED" | lolcat
+echo "MEMPENGARUHI PIKIRAN ANDA" | lolcat
+echo ""
+echo ""
+
